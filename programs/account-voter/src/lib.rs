37b540035345eac424888c1b2935e1f2987a812a
Boyquotes <webmaster@linux-live-cd.org>
2024-07-21 11:07:13 +0000

    Try String to store pseudo/mail in instruction

--- programs/account-voter/src/lib.rs
+++ programs/account-voter/src/lib.rs
@@ -7,7 +7,7 @@ declare_id!("6dPHkxoJGDyt4h37htgrm62zJU6aLdfHFcfX7KcHcyae");
 #[program]
 mod account_voter {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>, pseudo: [u8; 256], mail: [u8; 256], balance_total: u16, balance_sol: u16, total_trade: u16, total_participation: u16, win_trade: u16) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, pseudo: String, mail: String, balance_total: u16, balance_sol: u16, total_trade: u16, total_participation: u16, win_trade: u16) -> Result<()> {
         ctx.accounts.new_account.pseudo = pseudo;
         ctx.accounts.new_account.mail = mail;
         ctx.accounts.new_account.balance_total = balance_total;
